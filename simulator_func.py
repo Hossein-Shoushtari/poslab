6c173dc6288fd351b4191509b25ca352e8d5cb64
Korvin Venzke <korvin.venzke@gmail.com>
2022-01-15 11:27:40 +0100

    Korvin

--- simulator_func.py
+++ simulator_func.py
@@ -28,9 +28,9 @@ def get_map() -> 'html.Iframe':
         GeoJson(
             f'assets/floorplans/{geojson_file}',
             name=name,
-            tooltip=tooltips,
-            style_function=lambda x: {'weight': 0.5,'color': 'blue'},
-            highlight_function=lambda x: {'weight': 1,'color': 'orange'},
+            #tooltip=tooltips,
+            #style_function=lambda x: {'weight': 0.5,'color': 'blue'},
+            #highlight_function=lambda x: {'weight': 1,'color': 'orange'},
             show=False
         ).add_to(base_map)
         
