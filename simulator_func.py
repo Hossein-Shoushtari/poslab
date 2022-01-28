d3e45449a769e4df64623e498ab751926ba5692a
Dorian Harder <dorian.harder@hcu-hamburg.de>
2022-01-28 10:14:53 +0100

    added groundtrouth and file with example antennapositions to assets and inserted simulation function for 5G points

--- simulator_func.py
+++ simulator_func.py
@@ -6,6 +6,11 @@ from base64 import b64decode
 from maps import _map
 from math import atan2, pi, sqrt
 from json import loads
+import numpy as np
+import csv
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely.geometry import*
 
 def get_map() -> 'html.Iframe':
     '''
@@ -88,3 +93,77 @@ def azimuth(ax: float, ay: float, bx: float, by: float) -> float:
         return atan2((delta_y), (delta_x)) + 2*pi
     else:
         return atan2((delta_y), (delta_x))
+
+def simulate_positions(filename, error, freq):
+    """
+        creating sample data points for 5G measurements with chosen frequency and position error
+
+        Parameters
+        ----------
+        filename : name of csv file containing groundtrouth data points - string
+        error : desired error for 5G positions (as std) - float
+        freq : frequency of 5G measurements in (number of measurements/second)  - float
+
+        Returns
+        -------
+        positions : simulated positions from 5G measurements - array of arrays (tuples) of floats
+        time_stamps : timestamps of 5G measurements - array of floats
+        """
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        rows = []
+        positions = []
+        time_stamps = []
+        freq = freq / 1000
+        for row in reader:
+            rows.append([float(row[0].split(' ')[0]), float(row[0].split(' ')[1]), float(row[0].split(' ')[2])])
+
+        for r in range(len(rows)):
+            time_stamps.append(rows[r][0])
+            x_error = np.random.normal(0, error)
+            y_error = np.random.normal(0, error)
+            positions.append([rows[r][1] + x_error, rows[r][2] + y_error])
+
+            dt = 1 / freq
+            t1 = rows[r][0]
+            t2 = rows[r][0] + dt
+
+            if r <= len(rows) - 2:
+                dx = ((rows[r + 1][1] - rows[r][1]) / (rows[r + 1][0] - rows[r][0])) * dt
+                dy = ((rows[r + 1][2] - rows[r][2]) / (rows[r + 1][0] - rows[r][0])) * dt
+                # dz = ((rows[r-1][3] - rows[r][3])/(rows[r-1][0] - rows[r][0]))*dt
+
+                x_temp = rows[r][1] + dx
+                y_temp = rows[r][2] + dy
+                # z_temp = rows[r][3] + dz
+
+            while t2 < rows[-1][0] and t2 < rows[r + 1][0]:
+                time_stamps.append(t2)
+                x_error = np.random.normal(0, error)
+                y_error = np.random.normal(0, error)
+                positions.append([x_temp + dx + np.random.uniform(-error, error, 1)[0],
+                                  y_temp + dy + np.random.uniform(-error, error, 1)[0]])
+
+                t2 = t2 + dt
+                x_temp = x_temp + dx
+                y_temp = y_temp + dy
+
+        return positions, time_stamps
+
+if __name__ == "__main__":
+    """
+            check if the simulate_positions function is working
+            """
+
+    filename = 'assets/waypoints/GroundTruthZero2Four.csv'
+    error = 2
+    freq = 1
+    positions, time_stamps = simulate_positions(filename, error, freq)
+
+    points_positions = []
+    for p in positions:
+        points_positions.append(Point(p))
+
+    print(points_positions)
+    gpd.GeoSeries(points_positions).plot(figsize=(10, 10), color='red', markersize=100, label='5G positions')
+    plt.pause(60)
