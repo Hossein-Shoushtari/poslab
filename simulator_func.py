0a0538f813c506918c5a7a976b17538691d20f76
Dorian Harder <dorian.harder@hcu-hamburg.de>
2022-02-02 09:28:18 +0100

    implementing the option to use randomly generated semantic errors in the simulation function. Range of the number and possible lengths of the segments as well as the range for possible semantic error values can be chosen

--- simulator_func.py
+++ simulator_func.py
@@ -169,42 +169,103 @@ def closest_value(input_list, input_value):
     return arr[i]
 
 
-def simulate_positions(filename, error, freq):
+def semantic_error(number_range, error_range, intervall_range, time_stamps):
     """
-    creating sample data points for 5G measurements with chosen frequency and position error
+        creating semantic error values. the number of occurences, the error values and the length of each intervall of
+        semantic errors is randomly chosen from a given range
 
-    Parameters
-    ----------
-    filename : name of csv file containing groundtrouth data points - string
-    error : desired error for 5G positions (as std) - float
-    freq : frequency of 5G measurements in (number of measurements/second)  - float
+        Parameters
+        ----------
+        number_range : range of number of accurances of semantic errors - tuple of integers
+        error_range : range of possible error values for the semantic error - tuple of floats
+        intervall_range : range of possible lengths for the semantic errors - tuple of floats
+        time_stamps : time stamps from the ground truth data - list of floats
 
-    Returns
-    -------
-    positions : simulated positions from 5G measurements - array of arrays (tuples) of floats
-    time_stamps : timestamps of 5G measurements - array of floats
-    error_list : list of the error for each measurement - list of floats
-    qualities_list : list of the quality value for each measurement - list of floats
+        Returns
+        -------
+        intervall_lengths : length of all intervalls with semantic error - list of floats
+        errors : standard deviations for each semantic error intervall - list of floats
+        intervall_starts : start time of each semantic error intervall - list of floats
+        """
+    number_of_intervalls = np.random.randint(number_range[0], number_range[-1])
+    intervall_lengths = []
+    intervall_starts = []
+    errors = []
+
+    for i in range(number_of_intervalls):
+        intervall_lengths.append(np.random.uniform(intervall_range[0], intervall_range[
+            -1]))  # choose random intervalls (according to number of intervalls) from the timeline
+        intervall_starts.append(np.random.choice(time_stamps[:-1]))
+        errors.append(np.random.uniform(error_range[0], error_range[-1]))
+
+    return intervall_lengths, errors, intervall_starts
+
+
+def simulate_positions(filename, error, freq, number_range, error_range, intervall_range, semantic):
     """
+        creating sample data points for 5G measurements with defined frequency and measurement error range. Semantic errors are
+        generated in rondomly with chosen ranges for the duration, standard deviation and number of accurances
+
+        Parameters
+        ----------
+        semantic : if True use semantic errors, if False don't use semantic errors - Boolean
+        filename : name of csv file containing groundtrouth data points - string
+        error : desired error for 5G positions  - float
+        freq : frequency of 5G measurements in (number of measurements/second) - float
+        number_range : range of number of accurances of semantic errors - tuple of integers
+        error_range : range of possible error values for the semantic error - tuple of floats
+        intervall_range : range of possible lengths for the semantic errors - tuple of floats
+
+        Returns
+        -------
+        positions : simulated positions from 5G measurements - array of arrays (tuples) of floats
+        time_stamps : timestamps of 5G measurements - array of floats
+        error_list : list of the errors for each measurement - array of floats
+        qualities_list : estimated qualities for each measurement (as closest value from an list of quality values to the real error) - array of floats
+        """
     with open(filename, newline='') as f:
         reader = csv.reader(f)
         rows = []
         positions = []
         time_stamps = []
+
         freq = freq / 1000
+
         error_list = []
         for row in reader:
             rows.append([float(row[0].split(' ')[0]), float(row[0].split(' ')[1]), float(row[0].split(' ')[2])])
 
+        original_time_stamps = [r[0] for r in rows]
+        intervall_lengths, semantic_errors, intervall_starts = semantic_error(number_range, error_range,
+                                                                              intervall_range, original_time_stamps)
+        count = 0
+        semantic_active = False
+
         for r in range(len(rows)):
+
             time_stamps.append(rows[r][0])
-            x_error = np.random.normal(0, error)
-            y_error = np.random.normal(0, error)
+
+            if semantic == True:
+                if rows[r][0] >= intervall_starts[count] and rows[r][0] <= intervall_starts[count] + intervall_lengths[
+                    count]:
+                    current_error = semantic_errors[count]
+                    semantic_active == True
+
+                elif semantic_active == True:
+                    current_error = semantic_errors[count]
+                    count += 1
+                elif semantic_active == False:
+                    current_error = error
+            else:
+                current_error = error
+
+            x_error = np.random.normal(0, current_error)
+            y_error = np.random.normal(0, current_error)
             error_list.append(np.sqrt(x_error ** 2 + y_error ** 2))
             positions.append([rows[r][1] + x_error, rows[r][2] + y_error])
 
             dt = 1 / freq
-            t1 = rows[r][0]
+            #t1 = rows[r][0]
             t2 = rows[r][0] + dt
 
             if r <= len(rows) - 2:
@@ -218,21 +279,18 @@ def simulate_positions(filename, error, freq):
 
             while t2 < rows[-1][0] and t2 < rows[r + 1][0]:
                 time_stamps.append(t2)
-                x_error = np.random.normal(0, error)
-                y_error = np.random.normal(0, error)
+                x_error = np.random.normal(0, current_error)
+                y_error = np.random.normal(0, current_error)
                 error_list.append(np.sqrt(x_error ** 2 + y_error ** 2))
-                positions.append([x_temp + dx + x_error,
-                                  y_temp + dy + y_error])
-
+                positions.append([x_temp + dx + np.random.uniform(-current_error, current_error, 1)[0],
+                                  y_temp + dy + np.random.uniform(-current_error, current_error, 1)[0]])
                 t2 = t2 + dt
                 x_temp = x_temp + dx
                 y_temp = y_temp + dy
+
         qualities_list = []
-        # assigning the real error to the closest quality value (defined error/5). Might be more useful to
-        # assign the error to the intervall between two qualities or the next hiegher quality value
         for e in error_list:
-            intervalls = [error / 5, 2 * error / 5, 3 * error / 5, 4 * error / 5, 5 * error / 5]
-            quality = closest_value(intervalls, e)
+            quality = closest_value([error / 5, 2 * error / 5, 3 * error / 5, 4 * error / 5, 5 * error / 5], e)
             qualities_list.append(quality)
 
         return positions, time_stamps, error_list, qualities_list
@@ -245,7 +303,11 @@ if __name__ == "__main__":
     filename = 'assets/waypoints/GroundTruthZero2Four.csv'
     error = 2
     freq = 1
-    positions, time_stamps, errors, qualities = simulate_positions(filename, error, freq)
+    number_range = [1, 10] # range of number of occurencies for segments with semantic error
+    error_range = [1, 15] # range of possible values for the semantic errors
+    intervall_range = [1 * 1000, 20 * 1000] # range of intervall lengths where semantic errors are generated
+    semantic_is_used = False # set this to True if semantic errors are used
+    positions,time_stamps, errors, qualities = simulate_positions(filename,2,1,number_range, error_range, intervall_range,semantic_is_used)
     print('qualities',qualities)
     print('errors', errors)
 
